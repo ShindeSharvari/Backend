4c2be29b867d9e405704013d43b8522c8fad6629
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-08-07 13:54:59 +0530

    Add files via upload

---
+++ c/String/ToGetLengthOfString.c
@@ -0,0 +1,19 @@
+
+
+// Making string upperCase
+
+int getLengthOfString(char str[])
+{
+    int count=0;
+    for(int i=0;str[i]!='\0';count++,i++);
+
+    return count;
+}
+void main()
+{
+    char name[50];
+    printf("Enter your name: ");
+    gets(name);
+
+    printf("The length of %s = %d",name,getLengthOfString(name));
+}
