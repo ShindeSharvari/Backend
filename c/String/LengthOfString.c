7041135eddcbc26fe2a210b79c89acaeb077422d
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-08-08 19:29:24 +0530

    Add files via upload

---
+++ c/String/LengthOfString.c
@@ -0,0 +1,20 @@
+
+
+// Write a C program to find the length of a string.
+
+int getLengthOfString(char str[])
+{
+    int count=0;
+    for(int i=0;str[i]!='\0';count++,i++);
+
+    return count;
+}
+void main()
+{
+    char name[50];
+    printf("Enter your name: ");
+    gets(name);
+
+    printf("The length of %s = %d",name,getLengthOfString(name));
+}
+
