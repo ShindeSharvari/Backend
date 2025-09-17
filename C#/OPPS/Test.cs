7920bf6ed90a1e11e6bf5a2c6e571ed30dafa813
Sharvari Sunil  Shinde <135091984+ShindeSharvari@users.noreply.github.com>
2025-09-17 14:05:10 +0530

    Add files via upload

---
+++ C#/OPPS/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS
+{
+    public class Test
+    {
+        public int x = 0;
+        public static int cnt= 0;
+
+        public Test() { x++; cnt++; }
+
+        public Test(int x)
+        {
+            this.x = x;
+            cnt++;
+        }
+    }
+    public class TestApp
+    {
+        public static void Task()
+        {
+            Test t1 = new Test();
+            Console.WriteLine($"Count = {Test.cnt}");
+            Test t2 = new Test(11);
+            Console.WriteLine($"Count = {Test.cnt}");
+
+            Console.WriteLine($"t1 X = {t1.x}");
+            Console.WriteLine($"t2 X = {t2.x}");
+        }
+    }
+}
