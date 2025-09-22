b83b0a6ac3530a890041b8c9b1619325dc2e743c
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-09-22 21:31:49 +0530

    Add files via upload

---
+++ C#/OPPS/Inheritance/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Inheritance
+{
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} ,{Name}";
+        }
+
+    }
+
+    class Student : Person
+    {
+
+        public double Marks { get; set; }
+
+        public override string ToString()
+        {
+            return $"Student[ {base.ToString()}, {this.Marks}]";
+        }
+
+        public void F1() { }
+    }
+    class Teacher : Person
+    {
+
+
+        public double Salary { get; set; }
+        public override string ToString()
+        {
+            return $"Techer[{base.ToString()} , {Salary}]";
+        }
+    }
+
+    public  class FileDemo
+        {
+            public static void Task()
+            {
+            Person s = new Student() { Id = 1, Name = "Sharvari", Marks = 78 };
+            Console.WriteLine(s);
+            // s.F1();
+        }
+    }
+}
