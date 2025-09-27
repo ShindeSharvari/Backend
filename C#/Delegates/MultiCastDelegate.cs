8bb63049e80d92cc380354351f5d27de0098f560
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-09-27 20:16:08 +0530

    Add files via upload

---
+++ C#/Delegates/MultiCastDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Delegates
+{
+    public delegate void TaskDelegate();
+    public class MultiCastDelegate
+    {
+        public static void StartTask() => Console.WriteLine("Task Started");
+        public static void EndTask() => Console.WriteLine("Task Ended");
+    }
+
+    public class MultiDelegateTask
+    {
+        public static void Run()
+        {
+            TaskDelegate task = MultiCastDelegate.StartTask;
+
+            task += MultiCastDelegate.EndTask;
+            Console.WriteLine(task);
+        }
+    }
+}
