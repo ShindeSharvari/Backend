034a812bf3e9eefab8d9d4e0e3776eed44ec5f86
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-09-27 20:15:39 +0530

    Add files via upload

---
+++ C#/Delegates/Greetings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Delegates
+{
+    public delegate void GreetingDelegate(string name);
+    public class Greetings
+    {
+        public static void GoodMorning(string name)
+        {
+            Console.WriteLine($"Good Morning, {name}!");
+        }
+
+        public static void GoodAfternoon(string name)
+        {
+            Console.WriteLine($"Good Afternoon, {name}!");
+        }
+
+        public static void GoodEvening(string name)
+        {
+            Console.WriteLine($"Good Evening, {name}!");
+        }
+    }
+
+    public class Greet
+    {
+        public static void Task()
+        {
+            GreetingDelegate greetMorning = Greetings.GoodMorning;
+            GreetingDelegate greetAfternoon = Greetings.GoodAfternoon;
+            GreetingDelegate greetEvening = Greetings.GoodEvening;
+
+            greetMorning("Sharvari");
+            greetAfternoon("Sharvari");
+            greetEvening("Sharvari");
+
+            Console.ReadLine();
+        }
+    }
+}
