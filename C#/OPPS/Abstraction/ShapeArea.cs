41950924b16a76aba36409f73dc5b178ed17b94e
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-10-06 20:31:09 +0530

    Add files via upload

---
+++ C#/OPPS/Abstraction/ShapeArea.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Abstraction
+{
+    public abstract class Shapes
+    {
+
+        public abstract double CalculateArea(); 
+    }
+
+    public class Rectangle : Shapes
+    {
+        private double length;
+        private double width;
+        public Rectangle() { }
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+    public class Circle : Shapes
+    {
+        private double radius;
+
+        public Circle() { }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    public class ShapeArea
+    {
+        public static void Task()
+        {
+            Circle circle = new Circle(5.0);
+            Rectangle rectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine("Area of Circle: " + circle.CalculateArea());
+            Console.WriteLine("Area of Rectangle: " + rectangle.CalculateArea());
+        }
+    }
+}
---
+++ C#/OPPS/Abstraction/VehicleSimulation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Abstraction
+{
+    public abstract class Vehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Engine Started");
+        }
+
+        public abstract void Drive();
+    }
+    public class Bike : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Bike is ready to drive");
+        }
+    }
+    public class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Car is ready to drive");
+        }
+    }
+    public class VehicleSimulation
+    {
+        public static void Run()
+        {
+            Bike b1 = new Bike();
+            b1.StartEngine();
+            b1.Drive();
+
+            Car c1 = new Car();
+            c1.StartEngine();
+            c1.Drive();
+        }
+    }
+}
