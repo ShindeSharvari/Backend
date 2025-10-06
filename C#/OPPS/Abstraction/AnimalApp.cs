fff7f8bf32f2c3ef2d602938e6897a8986ec5922
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-10-06 20:29:58 +0530

    Add files via upload

---
+++ C#/OPPS/Abstraction/AnimalApp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Abstraction
+{
+
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+    }
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog barks");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cat Meows");
+        }
+    }
+    public class AnimalApp
+    {
+        public static void Task()
+        {
+            Dog d1 = new Dog();
+            d1.MakeSound();
+
+            Cat c1 = new Cat();
+            c1.MakeSound();
+        }
+    }
+}
