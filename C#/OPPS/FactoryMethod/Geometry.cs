90028ebdaf87940cdc6fa2ed1801f97e3dce3b82
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-10-10 18:58:22 +0530

    Add files via upload

---
+++ C#/OPPS/FactoryMethod/Geometry.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Interface.Factory
+{
+    public interface IShape
+    {
+        void Draw();
+    }
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("From Circle");
+        }
+    }
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("From Rectangle");
+        }
+    }
+    public class Triangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("From Triangle");
+        }
+    }
+    public static class ShapeFactory
+    {
+        public static IShape CreateShape(string shapeType)
+        {
+            switch (shapeType.ToLower())
+            {
+                case "circle":
+                    return new Circle();
+                case "rectangle":
+                    return new Rectangle();
+                case "triangle":
+                    return new Triangle();
+                default:
+                    Console.WriteLine("Invalid shape!");
+                    return null;
+            }
+        }
+    }
+
+
+        public class Geometry
+        {
+          public static void Task()
+          {
+            IShape shape = ShapeFactory.CreateShape("Triangle");
+            shape.Draw();
+
+            IShape shape2 = ShapeFactory.CreateShape("Hexagon");
+            shape2.Draw();
+          }
+        }
+}
