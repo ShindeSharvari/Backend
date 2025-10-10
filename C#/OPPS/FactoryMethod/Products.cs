9c7dbb5ae21aca483ae48be360684371eb2c2cf3
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-10-10 18:58:55 +0530

    Add files via upload

---
+++ C#/OPPS/FactoryMethod/Products.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Interface.Factory
+{
+    public interface IDocument
+    {
+        void Print();
+    }
+
+    public class WordDocument : IDocument
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printed from word document");
+        }
+    }
+    public class PdfDocument : IDocument
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printed from PDF document");
+        }
+    }
+    public class DocumentFactory
+    {
+        public static IDocument Create(string type)
+        {
+            string input = type.ToLower();
+
+            if (input == "word")
+            {
+                return new WordDocument();
+            }
+            else if (input == "pdf")
+            {
+                return new PdfDocument();
+            }
+            else
+            {
+                Console.WriteLine("Invalid Document");
+                return null;
+            }
+        }
+    }
+    public class Products
+    {
+        public static void Task()
+        {
+            IDocument d1 = DocumentFactory.Create("word");
+            d1.Print();
+        }
+    }
+}
---
+++ C#/OPPS/FactoryMethod/VehicleApp.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.OPPS.Interface.Factory
+{
+    public abstract class Vehicle
+    {
+        public abstract void Drive();
+    }
+
+    public class Car : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Let's go for a long drive by car");
+        }
+    }
+    public class Bike : Vehicle
+    {
+        public override void Drive()
+        {
+            Console.WriteLine("Let's go for a long drive on bike");
+        }
+    }
+
+    public abstract class VehicleFactory
+    {
+        public abstract Vehicle CreateVehicle();
+    }
+    public class CarFactory : VehicleFactory
+    {
+        public override Vehicle CreateVehicle()
+        {
+            return new Car();
+        }
+    }
+
+    public class BikeFactory : VehicleFactory
+    {
+        public override Vehicle CreateVehicle()
+        {
+            return new Bike();
+        }
+    }
+
+    public class VehicleApp
+    {
+        public static void Run()
+        {
+            // For car
+            VehicleFactory carFactory = new CarFactory();
+            Vehicle car = carFactory.CreateVehicle();
+            car.Drive();
+
+            //For bike
+            VehicleFactory bikeFactory = new BikeFactory();
+            Vehicle bike = bikeFactory.CreateVehicle();
+            bike.Drive();
+
+            // one - line calling 
+            new BikeFactory().CreateVehicle().Drive();
+
+        }
+    }
+}
