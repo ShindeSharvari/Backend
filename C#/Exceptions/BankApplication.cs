5bebce9ea75abb8c6c73e389689f3122b1a29a22
Sharvari Sunil  Shinde <shindesharvari003@gmail.com>
2025-10-11 18:58:08 +0530

    Add files via upload

---
+++ C#/Exceptions/BankApplication.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic.Exceptions
+{
+    public class NegativeAmountException : Exception
+    {
+        public NegativeAmountException(string message) : base(message) 
+        {
+        }
+    }
+    public class BankAccount
+    {
+        public int Balance { get; set; } = 1000;
+
+        public void Withdraw(int amount)
+        {
+            try
+            {
+                if(amount > Balance)
+                {
+                    throw new InvalidOperationException("InSufficient Balance");
+                }
+                if(amount < 0)
+                {
+                    throw new NegativeAmountException("Amount can't be negative");
+                }
+                Balance -= amount;
+                Console.WriteLine($"✅ Withdrawal successful! Remaining balance: ₹{Balance}");
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    public class BankApplication
+    {
+        public static void Run()
+        {
+            BankAccount moneyWithdraw = new BankAccount();
+            try
+            {
+                Console.WriteLine("Enter the amount to withdraw:");
+                int amount = int.Parse(Console.ReadLine());
+                moneyWithdraw.Withdraw(amount);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter a valid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for banking");
+            }
+        }
+    }
+}
